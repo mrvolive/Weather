53a5316f99089ba990fc978801967d92fa7e2c07
Olivier Maraval <olivier.maraval@edu.univ-fcomte.fr>
2025-02-11 15:01:43 +0100

    add some stuff and emojis

--- Weather/ContentView.swift
+++ Weather/ContentView.swift
@@ -17,10 +17,43 @@ struct ContentView: View {
                     .font(.largeTitle)
                     .bold()
                     .foregroundStyle(.white)
+                
                 Image(systemName: "cloud.sun.fill")
                     .renderingMode(.original)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 180, height: 180)
+                
+                Text("20°")
+                    .font(.system(size: 70,
+                                  weight: .medium))
+                    .foregroundStyle(.white)
+                
+                HStack{
+                    WeatherDayView(📆: "Mon", 🌆: "sun.max.fill", 🌡️: "25°")
+                }
             }
         }
+    }
+}
+
+struct WeatherDayView: View {
+    var 📆: String
+    var 🌆: String
+    var 🌡️: String
+    
+    var body: some View {
+        VStack {
+            Text(📆)
+                .foregroundStyle(.white)
+            
+            Image(systemName: "cloud.sun.fill")
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 40, height: 40)
+        }
+        
     }
 }
 
